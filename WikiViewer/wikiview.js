fe575f92d4e8d02b4f30ea8ef7f2ffb7d8da76d2
Bahnzo <bahnzo2020@gmail.com>
2017-03-06 16:55:42 -0700

    Final Code

--- WikiViewer/wikiview.js
+++ WikiViewer/wikiview.js
@@ -6,13 +6,51 @@ function wikiSearch() {
         url: 'http://en.wikipedia.org/w/api.php?',
         data: { action: 'opensearch', search: x, format: 'json' },
         dataType: 'jsonp',
-        success: displayResults(data)
+        success: function(data) {
+            displayResults(data);
+        }
     });
 }
 // $('#myForm').on('submit', wikiSearch);
-
 function displayResults(x) {
-    alert(JSON.stringify(y[1][1]));
+    // alert(JSON.stringify(x[1][1]));
+    var len = x[1].length;
+    document.getElementById("random-page").style.display = 'none';
+    document.getElementById("main").style.top = '20%';
+    document.getElementById("main").style.height = '75px';
+    var newDiv = document.querySelector('.second');
+    var hold;
+
+    for (var i = 0; i < len; i++) {
+        var s = "<div id='new-div" + [i] + "' style='border-style: solid;border-radius:5px'><p id='title-text'></p><p id='desc-text'></p></div>";
+        // var s = "<div id='new-div" + [i] + "' style='border-style: solid;border-radius:5px'><a href='#' id='title-text'></a><p id='desc-text'></p></div>";
+        var div = document.createElement('div');
+        div.innerHTML = s;
+        document.getElementById("second").appendChild(div);
+
+    }
+    for (i = 0; i < len; i++) {
+        var elem = '#new-div' + i;
+        var z = document.querySelector(elem);
+        var title = '<a href="' + x[3][i] + '">' + x[1][i] + '</a>';
+        /*document.querySelector(elem + " #title-text").textContent = x[1][i];
+        document.querySelector(elem + " #title-text").href = x[3][i];*/
+        document.querySelector(elem + " #title-text").innerHTML = title;
+        document.querySelector(elem + ' #desc-text').textContent = x[2][i];
+        z.style.margin = '5px';
+    }
+    newDiv.style.width = '50%';
+    newDiv.style.display = "block";
+    newDiv.style.margin = '0 auto';
+
+
+
+    /*document.body.appendChild(div);
+    var txt = document.createTextNode("New Div I created");
+    div.appendChild(txt);
+    div.innerHTML = s;*/
+
+
 }
 
 $("#search-input").keydown(function(event) {
